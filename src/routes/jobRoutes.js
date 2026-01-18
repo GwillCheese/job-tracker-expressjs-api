5cee6ad8a435980f1e4fe7406401aade1fd04e98
GwillCheese <danielkaku555@gmail.com>
2026-01-18 14:57:20 -0800

    jobRoutes minor bug fix

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const { getJobById } = require("../controllers/jobController");
 const {
-
     createJob,
     getJobs,
     getJobById,
